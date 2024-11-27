c9a8c183a812204771803398557f3149f2c025c8
Rikard Karlsson <karlsson0214@gmail.com>
2024-11-27 16:58:24 +0100

    Adding {} to if-statements.

--- EasyGame.cs
+++ EasyGame.cs
@@ -95,11 +95,16 @@ namespace EasyMonoGame
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
 
             // TODO: Add your update logic here
             if (activeWorld != null)
+            {
                 activeWorld.Update(gameTime);
+            }
+                
 
             base.Update(gameTime);
         }
