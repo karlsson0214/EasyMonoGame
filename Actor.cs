44c3886c50abfa416d6d89e92a68fb15ba2f9b57
Rikard Karlsson <karlsson0214@gmail.com>
2024-12-16 18:16:49 +0100

    Remove unnecessary  using.

--- Actor.cs
+++ Actor.cs
@@ -2,12 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 using Vector2 = Microsoft.Xna.Framework.Vector2;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace EasyMonoGame 
 {
